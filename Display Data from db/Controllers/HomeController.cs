b93a439168e106a66e8832c63356a35dc1e425c9
Adrian Soto <sotobraun@gmail.com>
2021-06-06 00:27:23 +0000

    dataGrid display

--- Display Data from db/Controllers/HomeController.cs
+++ Display Data from db/Controllers/HomeController.cs
@@ -6,21 +6,71 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Display_Data_from_db.Controllers
 {
+ 
     public class HomeController : Controller
     {
+        SqlCommand com = new SqlCommand();
+         private SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        
+        List<Address> addresses = new List<Address>();
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            con.ConnectionString = Display_Data_from_db.Properties.Resources.ConnectionSring;
         }
 
         public IActionResult Index()
         {
-            return View();
+            FetchData();
+
+            return View(addresses);
+        }
+
+        private void FetchData() 
+        {
+            if(addresses.Count > 0)
+            {
+                addresses.Clear();
+
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) [AddressID],[AddressLine1],[AddressLine2],[City],[StateProvinceID],[PostalCode],[SpatialLocation],[rowguid],[ModifiedDate] FROM[AdventureWorks2019].[Person].[Address]";
+                dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    addresses.Add(new Address() { AddressID = dr["AddressID"].ToString() 
+                    ,AddresLine = dr["AddressLine1"].ToString()
+                    ,City = dr["City"].ToString()
+                    ,StateProviceID = dr["StateProvinceID"].ToString()
+                    ,PostalCode = dr["PostalCode"].ToString()
+                    ,SpatialLocation = dr["SpatialLocation"].ToString()
+                    ,RowID = dr["rowguid"].ToString()
+                    ,ModifiedDate = dr["ModifiedDate"].ToString()
+
+                    });
+
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
         public IActionResult Privacy()
---
+++ Display Data from db/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Display_Data_from_db.Models
+{
+    public class Address
+    {
+        public string AddressID { get; set; }
+
+        public string AddresLine { get; set; }
+
+        public string City { get; set; }
+
+        public string StateProviceID { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public String SpatialLocation { get; set; }
+        public string RowID { get; set; }
+        public string ModifiedDate { get; set; }
+    }
+}
